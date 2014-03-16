654ef88309a141f6f54bb0a529baf021193a643d
barryloper <barry@loper.cc>
2014-03-16 12:11:09 -0600

    prob2 now right

--- euler2.py
+++ euler2.py
@@ -2,12 +2,12 @@
 
 f1 = 1
 f2 = 2
-fsum = 2
+fsum = 0
 evens = ("0", "2", "4", "6", "8") # I think i%2 could get expensive for large nums. -Not that this will get that large, now that I think about it...
 while f2 < 4000000:
     if str(f2)[-1:] in evens:
         fsum += f2
-        #print "%d, " % f2,
+        print "+ %d = %d, " % (f2, fsum)
     ft = f1 + f2 # a temp. to store next f
     f1 = f2 # shift the computed numbers down
     f2 = ft
